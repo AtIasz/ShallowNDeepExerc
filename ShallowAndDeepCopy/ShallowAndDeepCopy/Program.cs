5ebd0df439b3826d2daaaa4927835e9890ed1dd7
Atlasz <55188763+AtIasz@users.noreply.github.com>
2020-02-12 15:15:44 +0100

    Add files via upload
    Was a very painful and fun way to learn how to copy objects in different ways.

---
+++ ShallowAndDeepCopy/ShallowAndDeepCopy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShallowAndDeepCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list;
+
+            Recipe r1 = new Recipe("beer", list = new List<string>() {"water","alcohol" });
+            Recipe r2 = new Recipe("pizza", list = new List<string>() { "3,14159265","zza(?)" });
+            Recipe r3 = new Recipe("bolognese", list = new List<string>() {"noodles","tomato sauce","crushed meat", "spices"});
+            Recipe r4 = new Recipe("horse C**", list = new List<string>() {"speed","4 long legs (+1)","oat","2 ahyes" });
+            Recipe r5 = new Recipe("number 5", list = new List<string>() {"5 what?","4","3..." });
+            Console.WriteLine(r5.ToString()+"\n\n\n");
+            
+            Recipe r6 = (Recipe)r5.Clone();
+            r6.ingredients.Clear();
+            
+            Console.WriteLine(r5);
+            Console.WriteLine(r6);
+        }
+    }
+}
---
+++ ShallowAndDeepCopy/ShallowAndDeepCopy/Recipe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShallowAndDeepCopy
+{
+    class Recipe :ICloneable
+    {
+        private int id;
+        private string name;
+        public List<string> ingredients = new List<string>();
+
+        public Recipe()
+        {
+
+
+        }
+        public Recipe(string n,List<string>i)
+        {
+            this.id = Random_ID();
+            this.name = n;
+            this.ingredients = i;
+
+        }
+        public override string ToString()
+        {
+            string mamamia =string.Join(", ", ingredients);
+            return $"ID:{id} \tname:{name}\tingredients:{mamamia}";
+        }
+        
+        private int Random_ID()
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, 100000);
+            return number;
+            
+        }
+
+        
+
+        public object Clone()
+        {
+            Recipe copy = new Recipe();
+            copy.id = this.id;
+            copy.name = this.name;
+            copy.ingredients = this.ingredients;
+            return copy;
+        }
+    }
+}
